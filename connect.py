151887e3746943d0e66ae1f2b6c28bd8332cf655
Sydwell Sewela <sewelasydwell@gmail.com>
2019-10-07 13:39:36 +0200

    New functionality

--- connect.py
+++ connect.py
@@ -1,13 +1,31 @@
-import program
+from program import get_filename_full_path, get_username_password, get_ap_port_info
+from netmiko import ConnectHandler
 import os
 
 
+def connect_to_device(name):
+    user = get_username_password(name)
+    ar1 = {
+        'device_type': 'cisco_ios',
+        'ip': '196.160.66.33',
+        'username': '{}'.format(user.username),
+        'password': '{}'.format(user.password)
+    }
+
+    connect = ConnectHandler(**ar1)
+
+    if not connect.is_alive():
+        print('Connection refused.')
+    else:
+        return connect
+
+
 def get_lldp_info(fname, name):
-    connect = program.connect_to_device(name)
+    connect = connect_to_device(name)
     print('Connecting to {}...'.format(connect.host))
     cmd = 'show lldp neighbors detail'
     # lines = list()
-    filename = program.get_filename_full_path(fname)
+    filename = get_filename_full_path(fname)
 
     if connect:
         output = connect.send_command(cmd)
@@ -21,9 +39,9 @@ def get_lldp_info(fname, name):
 
 
 def read_data(fname, name):
-    filename = program.get_filename_full_path(fname)
+    filename = get_filename_full_path(fname)
     lines = list()
-    con = program.connect_to_device(name)
+    con = connect_to_device(name)
 
     if os.path.exists(filename):
         with open(filename) as fin:
@@ -35,3 +53,20 @@ def read_data(fname, name):
     cmd = 'ping {}'.format(ip[1].strip())
     output = con.send_command(cmd)
     print(output)
+
+
+def get_ap_mac_address(filename):
+    # url = 'https://196.160.9.15:7443/api/public'
+    # send_request = requests.get(url)
+    # print(send_request.status_code)
+    filename = get_filename_full_path(filename)
+    data = []
+
+    if os.path.exists(filename):
+        with open(filename) as fout:
+            data = fout.readlines()
+
+    chassis_id = data[2]
+    mac_address = chassis_id.split(':')[1].strip()
+
+    return mac_address
--- program.py
+++ program.py
@@ -1,7 +1,8 @@
-from netmiko import ConnectHandler
 import collections
 import connect
 import os
+import csv
+import requests
 
 UserCredentials = collections.namedtuple('UserCredentials',
                                          'username, password')
@@ -14,53 +15,39 @@ def main():
     print()
     name = 'credentials'
     filename = 'lldp-info'
+    file = 'newfile'
     # get_filename_full_path(name)
-    config_device(name)
-    connect.get_lldp_info(filename, name)
-    connect.read_data(filename, name)
+    # config_device(name)
+    # connect.get_lldp_info(filename, name)
+    # connect.read_data(filename, name)
+
+    get_ap_port_info(filename, name, file)
 
 
 def config_device(name):
-    connect = connect_to_device(name)
+    conn = connect.connect_to_device(name)
     config_command = ''
     cmd = 'show run | include ntp server'
-    ping = 'ping {}'.format(connect.host)
+    ping = 'ping {}'.format(conn.host)
 
-    ping_output = connect.send_command(ping)
+    ping_output = conn.send_command(ping)
     print(ping_output)
 
-    print('Connecting to {}...'.format(connect.host))
+    print('Connecting to {}...'.format(conn.host))
 
-    if connect:
+    if conn:
         show_output = ''
         config_command = ['ntp server 196.26.5.10', 'ntp server 196.4.160.4']
 
         for config in config_command:
-            output = connect.send_config_set(config)
+            output = conn.send_config_set(config)
             print(output)
 
-        show_output = connect.send_command(cmd)
+        show_output = conn.send_command(cmd)
         print(show_output)
 
     else:
         print('Failed to send config "{}"'.format(config_command))
-
-
-def connect_to_device(name):
-    user = get_username_password(name)
-    ar1 = {
-        'device_type': 'cisco_ios',
-        'ip': '196.160.66.33',
-        'username': '{}'.format(user.username),
-        'password': '{}'.format(user.password)
-    }
-
-    connect = ConnectHandler(**ar1)
-
-    if not connect.is_alive():
-        print('Connection refused.')
-    else:
-        return connect
 
 
 def get_username_password(name):
@@ -88,6 +75,34 @@ def get_username_password(name):
 def get_filename_full_path(name):
     filename = os.path.abspath(os.path.join('.', 'Files', name + '.txt'))
     return filename
+
+
+def get_ap_port_info(fname, name, file):
+    filename = os.path.abspath(os.path.join('.', 'Files', file + '.csv'))
+    mac_address = connect.get_ap_mac_address(fname)
+    conn = connect.connect_to_device(name)
+    cmd = 'show mac-address-table address {}'.format(mac_address)
+
+    try:
+        if conn:
+            out = conn.send_command(cmd)
+            lines = out.split('\n')
+
+            line = lines[2].split('\t')
+            new_line = ''
+            for l in line:
+                if len(l) > 0:
+                    l = l.strip('\t')
+                    l = l.strip(' ')
+                    new_line = new_line + l + ' '
+
+        with open(filename, 'w') as fout:
+            csv_writer = csv.writer(fout)
+            new_line = new_line.split(' ')
+            csv_writer.writerow(new_line)
+
+    except ConnectionRefusedError:
+        print('Connection refused')
 
 
 if __name__ == '__main__':
